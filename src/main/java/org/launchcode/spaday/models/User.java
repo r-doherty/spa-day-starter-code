a53256e8af645c509ce3d3f5c38555262a5594d2
Ryan Doherty <rjd.atx@gmail.com>
2020-02-14 09:51:23 -0600

    13.6 studio -- pre bonus mission update

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,36 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotEmpty(message="Username field cannot be empty")
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters long")
     private String username;
+
+    @Email(message="Invalid email format, please try again")
     private String email;
+
+    @Size(min=6, message="Password must be at least six characters long")
+    @NotEmpty(message="Password field cannot be empty")
     private String password;
+
+    @NotEmpty(message="Verify field cannot be empty")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +55,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
